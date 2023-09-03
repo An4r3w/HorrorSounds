c910cfffa07f4279da9c6710617165f0edac406b
An4r3w <75725607+An4r3w@users.noreply.github.com>
2023-09-03 22:43:57 +0200

    Create EventHandler.cs

---
+++ HorrorSounds/EventHandler.cs
@@ -0,0 +1,44 @@
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Server;
+using MEC;
+using System;
+using System.Collections.Generic;
+
+namespace HorrorSounds
+{
+    public class EventHandler
+    {
+        private CoroutineHandle cassieCoroutine;
+        private List<string> customSounds;
+        public Random Random { get; } = new Random();
+
+        public void OnRoundStarted()
+        {
+            float delay = UnityEngine.Random.Range(60f, 120f);
+
+            cassieCoroutine = Timing.CallDelayed(delay, PlayRandomHorrorSound);
+            Log.Debug("the corountine has started.");
+        }
+
+        public void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            Timing.KillCoroutines(cassieCoroutine);
+            Log.Debug("the corountine has been killed.");
+        }
+
+        public void PlayRandomHorrorSound()
+        {
+            customSounds = Plugin.Instance.Config.CustomSounds;
+
+            if (customSounds.Count > 0)
+            {
+                int randomIndex = UnityEngine.Random.Range(0, customSounds.Count);
+                Cassie.Message(message: customSounds[randomIndex], isNoisy: false, isSubtitles: false);
+                Log.Debug("a horror sound has been played");
+            }
+
+            float nextDelay = UnityEngine.Random.Range(Plugin.Instance.Config.MinDelaySeconds, Plugin.Instance.Config.MaxDelaySeconds);
+            cassieCoroutine = Timing.CallDelayed(nextDelay, PlayRandomHorrorSound);
+        }
+    }
+}
